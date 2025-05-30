c9f39d2de4ab72a95c291403750ca4670c2e628b
AlonCohen96 <aloncohen36@gmail.com>
2025-05-30 16:01:23 +0200

    test commit

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -15,7 +15,7 @@ import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
-// some comment to test
+// some test comment
 
 const GRAPHQL_URL =
     process.env.NODE_ENV === "production"
