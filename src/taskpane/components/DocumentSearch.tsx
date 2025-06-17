d354377dc244dd6dde4c9c1910cc007e72d00943
AlonCohen96 <aloncohen36@gmail.com>
2025-06-17 17:19:52 +0200

    Added title size limitation for titles longer than 100 characters. Added Read window to read paper and view additional metadata. Redesigned paper boxes.

--- src/taskpane/components/DocumentSearch.tsx
+++ src/taskpane/components/DocumentSearch.tsx
@@ -6,11 +6,23 @@ import { DOCUMENT_SEARCH, PAGINATED_SEARCH, ADD_PAPER_TO_ZOTERO } from '../schem
 import { toast } from 'react-toastify';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+import Tooltip from "@mui/material/Tooltip";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import IconButton from "@mui/material/IconButton";
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import CloseIcon from '@mui/icons-material/Close';
+
+const loremIpsum = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
 interface Paper {
     title: string;
     authors: any[];
     year: number;
+    abstract: string;
+    fullPaper: string
     collection: string;
     id_field: number;
     id_type: number;
@@ -22,6 +34,7 @@ const DocumentSearch = () => {
     const [foundPapers, setFoundPapers] = useState<Paper[] | null>(null);
     const [keywords, setKeywords] = useState('');
     const [loadingBar, setloadingBar] = useState(false);
+    const [expandedPaper, setExpandedPaper] = useState<Paper | null>(null);
 
     const [getDocuments, { loading: loadingDocs, error: errorDocs, data: dataDocs }] = useLazyQuery(DOCUMENT_SEARCH);
     const [getPaperMeta, { loading: loadingMeta, error: errorMeta, data: dataMeta }] = useLazyQuery(PAGINATED_SEARCH);
@@ -68,6 +81,8 @@ const DocumentSearch = () => {
                     title: paper.Title,
                     authors: paper.Author,
                     year: paper.PublicationDate.Year,
+                    abstract: paper.Content.Abstract,
+                    fullPaper: loremIpsum,
                     collection: "S2AG",
                     idField: "id_int",
                     idType: "int",
@@ -112,6 +127,10 @@ const DocumentSearch = () => {
         }
     }
 
+    const handleClickReadPaper = (paper) => {
+        setExpandedPaper(paper)
+    }
+
 
     useEffect(() => {
         // Function to handle storage updates
@@ -135,6 +154,63 @@ const DocumentSearch = () => {
 
     return (
         <>
+            <Dialog
+                open={Boolean(expandedPaper)}
+                onClose={() => setExpandedPaper(null)}
+                fullWidth
+                maxWidth="md"
+            >
+                <DialogActions>
+                    <IconButton
+                        aria-label="Read paper"
+                        size="medium"
+                        sx={{
+                            width: 32,
+                            height: 32,
+                            borderRadius: "50%"
+                        }}
+                        onClick={() => handleClickZoteroBtn(expandedPaper)}
+                    >
+                        <img
+                            src="/assets/zotero-icon.ico"
+                            alt="Add to Zotero"
+                            width={20}
+                            height={20}
+                        />
+                    </IconButton>
+                    <IconButton
+                        aria-label="Read paper"
+                        size="medium"
+                        sx={{
+                            width: 32,
+                            height: 32,
+                            borderRadius: "50%"
+                        }}
+                        onClick={() => setExpandedPaper(null)}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                </DialogActions>
+                <DialogTitle sx={{ fontWeight: 'bold' }}>
+                    {expandedPaper?.title}
+                </DialogTitle>
+                <DialogContent>
+                    <p>
+                        Authors:{' '}
+                        {expandedPaper?.authors.map(
+                            (author) => `${author?.FamilyName}, ${author?.GivenName[0]}. `
+                        )}
+                    </p>
+                    <p>Year: {expandedPaper?.year}</p>
+                </DialogContent>
+                <DialogContent dividers>
+                    <p>{expandedPaper?.abstract ? expandedPaper.abstract : 'Abstract not available for this paper.'} </p>
+                </DialogContent>
+                <DialogContent dividers>
+                    <p>{expandedPaper?.fullPaper ? expandedPaper.fullPaper : 'Fully body not available for this paper.'} </p>
+                </DialogContent>
+            </Dialog>
+
             <div className="search-container">
                 <p className="title">Discover</p>
 
@@ -171,14 +247,86 @@ const DocumentSearch = () => {
                 </Box>
             }
 
-            {foundPapers?.map(paper => (
-                <div className="result" key={nanoid()}>
-                    <h3>{paper.title}</h3>
-                    <p>Authors: {paper.authors.map(author => `${author?.FamilyName}, ${author?.GivenName[0]}. `)}</p>
-                    <p>Year: {paper.year}</p>
-                    <button onClick={() => handleClickZoteroBtn(paper)}>Add to Zotero</button>
-                </div>
-            ))}
+            {foundPapers?.map(paper => {
+                const titleTooLong = paper.title.length > 100;
+                const truncatedTitle = titleTooLong
+                    ? `${paper.title.substring(0, 100)}…`
+                    : paper.title;
+
+                return (
+                    <div className='paper-box' key={nanoid()}>
+                        <div className='paper-box-meta-container'>
+                            {titleTooLong ? (
+                                <Tooltip
+                                    title={paper.title}
+                                    componentsProps={{
+                                        tooltip: {
+                                            sx: {
+                                                fontSize: '1.2rem',
+                                                lineHeight: 1.4,
+                                            }
+                                        }
+                                    }}
+                                >
+                                    <h3>{truncatedTitle}</h3>
+                                </Tooltip>
+                            ) : (
+                                <h3>{truncatedTitle}</h3>
+                            )}
+
+                            <p>
+                                Authors:{' '}
+                                {paper.authors.map(
+                                    (author) => `${author?.FamilyName}, ${author?.GivenName[0]}. `
+                                )}
+                            </p>
+                            <p>Year: {paper.year}</p>
+                        </div>
+
+                        <div className='paper-box-buttons-container'>
+                            <Tooltip
+                                title={"Expand this paper."}
+                            >
+                                <IconButton
+                                    aria-label="Read paper"
+                                    size="medium"
+                                    sx={{
+                                        width: 32,
+                                        height: 32,
+                                        borderRadius: "50%"
+                                    }}
+                                    onClick={() => handleClickReadPaper(paper)}
+                                >
+                                    <MenuBookIcon />
+                                </IconButton>
+                            </Tooltip>
+
+                            <Tooltip
+                                title={"Add this paper to Zotero."}
+                            >
+                                <IconButton
+                                    aria-label="Read paper"
+                                    size="medium"
+                                    sx={{
+                                        width: 32,
+                                        height: 32,
+                                        borderRadius: "50%"
+                                    }}
+                                    onClick={() => handleClickZoteroBtn(paper)}
+                                >
+                                    <img
+                                        src="/assets/zotero-icon.ico"
+                                        alt="Add to Zotero"
+                                        width={20}
+                                        height={20}
+                                    />
+                                </IconButton>
+                            </Tooltip>
+                        </div>
+
+                    </div>
+                );
+            })}
         </>
     );
 
