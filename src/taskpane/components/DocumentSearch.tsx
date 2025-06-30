9950462da7a2099382b08b2c1d33f89e4cb70fb6
AlonCohen96 <aloncohen36@gmail.com>
2025-06-30 14:49:31 +0200

    Removed Lorem Ipsum as sample text

--- src/taskpane/components/DocumentSearch.tsx
+++ src/taskpane/components/DocumentSearch.tsx
@@ -17,8 +17,6 @@ import CloseIcon from '@mui/icons-material/Close';
 import GoogleScholarChip from "./GoogleScholarChip";
 import DOIChip from "./DOIChip";
 
-const loremIpsum = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-
 interface Paper {
     title: string;
     authors: any[];
@@ -85,7 +83,7 @@ const DocumentSearch = ({apiKey}) => {
                     authors: paper.Author,
                     year: paper.PublicationDate.Year,
                     abstract: paper.Content.Abstract,
-                    fullPaper: loremIpsum,
+                    fullPaper: null, //SINGLE_PAPER_QUERY
                     collection: "S2AG",
                     DOI: paper.DOI,
                     idField: "id_int",
